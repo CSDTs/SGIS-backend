39656a575ff11a34271d5d3e8e3cbb2360dd0fac
Kathleen Tully <tullyk@rpi.edu>
2014-09-09 10:28:20 -0400

    removing old names

--- gis/jobs/daily/dataset.py
+++
@@ -1,9 +0,0 @@
-from django_extensions.management.jobs import DailyJob
-from gis.models import Dataset
-
-class Job(DailyJob):
-    help = 'Daily dataset updater'
-    def execute(self):
-    	datasets = Dataset.objects.all()
-    	for dataset in datasets:
-    		dataset.update_mappoints()
