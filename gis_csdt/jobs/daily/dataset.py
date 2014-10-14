3b0d6383b44a16dfe8187ffb75744ebc3511b690
Kathleen Tully <tullyk@rpi.edu>
2014-10-14 14:35:05 -0400

    fixed nightly update script to not delete a bunch when unnecessary, to not update whole set if remote dataset has no unique id

--- gis_csdt/jobs/daily/dataset.py
+++ gis_csdt/jobs/daily/dataset.py
@@ -4,6 +4,12 @@ from gis_csdt.models import Dataset
 class Job(DailyJob):
     help = 'Daily dataset updater'
     def execute(self):
-    	datasets = Dataset.objects.all()
-    	for dataset in datasets:
-    		dataset.update_mappoints()
+        datasets = Dataset.objects.all()
+        for dataset in datasets:
+            print 'Update dataset:', dataset
+            try:
+                dataset.update_mappoints()
+            except Exception as inst:
+                print type(inst)     # the exception instance
+                print inst.args      # arguments stored in .args
+                print inst           # __str__ allows args to be printed directly
