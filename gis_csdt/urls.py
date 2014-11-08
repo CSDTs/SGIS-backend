57c3a68c34af16c3c1c76fc3db30f9e4f192c4e8
Kathleen Tully <kathleen.tully@gmail.com>
2014-11-08 17:46:22 -0500

    remove old around point view

--- gis_csdt/urls.py
+++ gis_csdt/urls.py
@@ -13,13 +13,14 @@ router.register(r'api-poly', views.MapPolygonViewSet, base_name = 'mappolygon')
 router.register(r'api-test', views.TestView)
 router.register(r'api-count', views.CountPointsInPolygonView)#, base_name = 'count_points')
 #router.register(r'api-tag/count', views.TagCountViewSet, base_name = 'tag')
+router.register(r'api-dist', views.AnalyzeAreaAroundPointView)
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     #url(r'^api-count/', views.CountPointsInPolygonView.as_view(), name='count'),
-    url(r'^api-dist/', views.AnalyzeAreaAroundPointView, name='distance'),
+    #url(r'^api-dist/', views.AnalyzeAreaAroundPointView, name='distance'),
     url(r'^around-point/(?P<mappoint_id>[0-9]+)/$', templates.AroundPointView),
     url(r'^around-point/$', templates.AroundPointView),
 )
