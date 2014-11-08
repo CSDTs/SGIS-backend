0f2186f11f692bfc46426d4d3bde05ca33bbea74
Kathleen Tully <kathleen.tully@gmail.com>
2014-11-08 17:45:35 -0500

    show 3 circles. Default to 1,45 (east of africa) to compare geometry in different locations. Remove debugging printouts. Don't redraw circles on moving the map

--- gis_csdt/templates.py
+++ gis_csdt/templates.py
@@ -1,15 +1,17 @@
 from django.template import Template, Context
 from gis_csdt.settings import GOOGLE_API_KEY
 from gis_csdt.models import MapPoint
+from gis_csdt.geometry_tools import circle_as_polygon
 from django.http import HttpResponse
 from django.contrib.sites.models import Site
+from django.contrib.gis.measure import Distance
 
 def AroundPointView(request, mappoint_id = None):
 	current_site = Site.objects.get_current()
 	context = {'key': GOOGLE_API_KEY,
 			   'zoom_level': 12,
-			   'lat': 42.7317,
-			   'lon': -73.6925,
+			   'lat': 1,
+			   'lon': 45,
 			   'width': 500,
 			   'height': 380,
 			   'root': current_site.domain + '/'}
@@ -17,20 +19,26 @@ def AroundPointView(request, mappoint_id = None):
 		context['lat'] = request.GET['lat']
 	if 'lon' in request.GET:
 		context['lon'] = request.GET['lon']
-	print mappoint_id
 	if mappoint_id:
 		try:
 			mappoint = MapPoint.objects.get(id = mappoint_id)
-			print 'here'
 			if mappoint.point.x != 0:
 				context['lon'] = mappoint.point.x
 			if mappoint.point.y != 0:
 				context['lat'] = mappoint.point.y
 		except Exception as e:
-			print e
+			pass
 
 	if context['root'] == 'example.com/':
 		context['root'] = 'http://127.0.0.1:8000/'
+	context['circle']=''
+	for d in [1,3,5]:
+		circle = circle_as_polygon(lat = context['lat'], lon = context['lon'], distance = Distance(km=d))
+		text = ''
+		for coord in circle[0]: #external ring
+			text = text + ',new google.maps.LatLng(%f, %f)\n' %(coord[1], coord[0])
+		context['circle'] = context['circle'] + ',['+text.strip(',')+']'
+	context['circle'] = '[' + context['circle'].strip(',') + ']'
 	context = Context(context)
 	template = """<!DOCTYPE HTML>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
@@ -40,18 +48,47 @@ def AroundPointView(request, mappoint_id = None):
 	<script>
 	var map;
 	function initialize() {
-	  function set_height(){
-	  	var h = $(window).height();
-        $('#googleMap').css('height', h);
-	  }
-	  $(window).resize(set_height());
-	  set_height();
-	  // Create a simple map.
-	  map = new google.maps.Map(document.getElementById('googleMap'), {
-	    zoom: {{ zoom_level }},
-	    center: {lat: {{ lat }}, lng:{{ lon }}}
-	  });
+		function add_circles(){
+		  	var center = new google.maps.LatLng({{ lat }}, {{ lon }});
+		  	for (var i=5;i>0;i-=2){
+			  	var circleOptions = {
+			      strokeColor: '#000000',
+			      strokeOpacity: 0.8,
+			      strokeWeight: 2,
+			      fillColor: '#FF0000',
+			      fillOpacity: 0.35,
+			      map: map,
+			      center: center,
+			      radius: i * 1000
+			    };
+			    // Add the circle for this city to the map.
+			    circle = new google.maps.Circle(circleOptions);
+			}
+		}
+		function add_polygons(){
+		  var polygon = new google.maps.Polygon({
+		    paths: {{ circle }},
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 0.8,
+		    strokeWeight: 2,
+		    fillColor: '#FF0000',
+		    fillOpacity: 0.35
+		  });
+		  polygon.setMap(map);
+		}
+		function set_height(){
+			var h = $(window).height();
+		$('#googleMap').css('height', h);
+		}
+		$(window).resize(set_height());
+		set_height();
+		// Create a simple map.
+		map = new google.maps.Map(document.getElementById('googleMap'), {
+			zoom: {{ zoom_level }},
+			center: {lat: {{ lat }}, lng:{{ lon }}}
+		});
 
+  		add_polygons();
 	  // Load a GeoJSON from the same server as our demo.
 	  google.maps.event.addListener(map, 'bounds_changed', function() {
 	  	load_polys("{{ root }}api-poly/?max_lat=" + map.getBounds().getNorthEast().lat() + "&max_lon=" + map.getBounds().getNorthEast().lng() + "&min_lat=" + map.getBounds().getSouthWest().lat() + "&min_lon="+ map.getBounds().getSouthWest().lng());
@@ -74,25 +111,7 @@ def AroundPointView(request, mappoint_id = None):
     strokeWeight: 1
   }
   map.data.setStyle(featureStyle);
-  add_circles();
 
-  function add_circles(){
-  	var center = new google.maps.LatLng({{ lat }}, {{ lon }})
-  	for (var i=5;i>0;i-=2){
-  	var circleOptions = {
-      strokeColor: '#000000',
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: '#FF0000',
-      fillOpacity: 0.35,
-      map: map,
-      center: center,
-      radius: i * 1000
-    };
-    // Add the circle for this city to the map.
-    cityCircle = new google.maps.Circle(circleOptions);
-}
-  }
 	  });
 
 	}
