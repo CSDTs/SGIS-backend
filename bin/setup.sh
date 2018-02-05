bd9bffa4df7eaf3a857540c19adba09ec17bfdd3
timothyw0 <timothyw0@gmail.com>
2018-02-05 16:45:34 +0000

    Fixed error messages

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,16 +4,16 @@
 echo "Getting essentials"
 echo
 echo
-sudo apt-get install python-dev build-essential
+sudo apt-get -y install python-dev build-essential
 
 # Pip and Virtualenv
 echo "Getting pip, virtualenv, and pip libraries"
 echo
 echo
-sudo apt-get install python-pip
+sudo apt-get -y install python-pip
 sudo pip install virtualenv
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install libpq-dev
+sudo apt-get -y install libcurl4-openssl-dev
+sudo apt-get -y install libpq-dev
 . activate
 pip install https://github.com/djangonauts/django-rest-framework-gis/tarball/master
 
@@ -29,6 +29,9 @@ sudo su - postgres -c createdb django_test exit
 echo "Installing GEOS"
 echo
 echo
+sudo apt-get -y install build-essential
+sudo apt-get -y install gcc build-essential
+sudo apt-get -y install g++ build-essential
 wget http://download.osgeo.org/geos/geos-3.4.2.tar.bz2
 tar xjf geos-3.4.2.tar.bz2
 cd geos-3.4.2
@@ -56,6 +59,7 @@ cd ..
 echo "Setting up PostGIS"
 echo
 echo
+cd ~/SGIS-backend/bin
 sudo su - postgres -c psql django_test < psqlCommands.sql
 
 echo "All done"
