08e542e059318c905e647f519db5fe0b8076887b
serenasyw <yawen.shen620@gmail.com>
2018-06-08 16:08:51 -0400

    add more tests for serializers

--- gis_csdt/tests/test_serializers.py
+++ gis_csdt/tests/test_serializers.py
@@ -107,7 +107,12 @@ class TestAnalyzeAreaSerializer(TestCase):
 		tagindiv1.save()
 		tagindiv2 = TagIndiv(tag=tag2, mapelement=self.mp)
 		tagindiv2.save()
-
+		# p1 = Polygon( ((0, 0), (3, 7), (10, 10), (0, 0)) )
+ 	# 	p2 = Polygon( ((1, 1), (1, 4), (15, 15), (1, 1)) )
+		# mpoly = MultiPolygon(p1, p2)
+		# polygon = MapPolygon(lat='50.2340', lon='28.3282', field1=1.0, field2=2.0, mpoly=mpoly, dataset=ds, remote_id=1)
+		# polygon.save()
+	
 		self.request = HttpRequest()
 		qdict = QueryDict('', mutable=True)
 		qdict.update({'year': '2014'})
@@ -118,7 +123,6 @@ class TestAnalyzeAreaSerializer(TestCase):
 	
 	def test_can_get_areaAroundPoint(self):
 		data = self.serializer.get_areaAroundPoint(self.mp)
-		print data
 		self.assertEqual(data['point id(s)'], '2')
 		self.assertTrue(data.get('1.000000 km'))
 		self.assertTrue(data.get('3.000000 km'))
@@ -126,8 +130,44 @@ class TestAnalyzeAreaSerializer(TestCase):
 
 	def test_can_get_areaAroundPoint2(self):
 		data = self.serializer.get_areaAroundPoint2(self.mp)
-		print data
-		self.assertEqual(data['point id(s)'], '3')
 		self.assertTrue(data.get('1.000000 km'))
 		self.assertTrue(data.get('3.000000 km'))
 		self.assertTrue(data.get('5.000000 km'))
+
+class TestAnalyzeAreaNoValuesSerializer(TestCase):
+	fixtures = ['test_data.json']
+
+	def setUp(self):
+		ds = Dataset(name="census2016")
+		ds.save()
+		self.mp = MapPoint.objects.get(pk=1)
+		self.mp.dataset = ds
+		self.mp.point = Point(5, 23)
+		MapPoint(lat=23.41, lon=98.0, dataset=ds, point=Point(5, 23), state='NY', city='NYC').save()
+		tag1 = Tag(dataset=ds, tag='tag1', approved=True, count=1)
+		tag1.save()
+		tag2 = Tag(dataset=ds, tag='tag2', approved=True, count=1)
+		tag2.save()
+		tagindiv1 = TagIndiv(tag=tag1, mapelement=self.mp)
+		tagindiv1.save()
+		tagindiv2 = TagIndiv(tag=tag2, mapelement=self.mp)
+		tagindiv2.save()
+		# p1 = Polygon( ((0, 0), (3, 7), (10, 10), (0, 0)) )
+ 	# 	p2 = Polygon( ((1, 1), (1, 4), (15, 15), (1, 1)) )
+		# mpoly = MultiPolygon(p1, p2)
+		# polygon = MapPolygon(lat='50.2340', lon='28.3282', field1=1.0, field2=2.0, mpoly=mpoly, dataset=ds, remote_id=1)
+		# polygon.save()
+	
+		self.request = HttpRequest()
+		qdict = QueryDict('', mutable=True)
+		qdict.update({'year': '2014'})
+		qdict.update({'year': '2016'})
+		qdict.update({'unit': 'km'})
+		self.request.GET = qdict
+		self.serializer = AnalyzeAreaNoValuesSerializer(context={'request': self.request})
+	
+	def test_can_get_areaAroundPoint(self):
+		data = self.serializer.get_areaAroundPoint(self.mp)
+		self.assertEqual(data['1.000000 km'], {})
+		self.assertEqual(data['points'][0]['state'], 'NY')
+		self.assertEqual(data['points'][0]['city'], 'NYC')
